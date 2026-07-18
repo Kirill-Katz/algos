02bf090d6fea5cf767fafda92863cc632a948244
Kirill Katz <xnlyre@protonmail.com>
2026-07-18 22:42:20 +0300

    SOlve

---
+++ cf_1987c.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+template <class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void solve();
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+
+
+    int ans = a[n - 1];
+
+    for (int i = n - 2; i >= 0; --i) {
+        ans = max(ans + 1, a[i]);
+    }
+
+
+    cout << ans << '\n';
+}
