15f4f1ccdfd1049b95e38b75ec366e52c32d3729
Kirill Katz <xnlyre@protonmail.com>
2026-08-30 21:14:22 +0300

    Solve

--- cf_2239c.cpp
+++ cf_2239c.cpp
@@ -23,15 +23,12 @@ void solve() {
     int n;
     cin >> n;
 
-    vector<int> vals(n);
-    vector<int> type(n);
+    vector<long long> vals(n);
+    vector<char> type(n);
 
     for (int i = 0; i < n; ++i) {
-        char c, v;
-        cin >> c >> v;
-
-        type[i] = c;
-        vals[i] = v;
+        cin >> type[i];
+        cin >> vals[i];
     }
 
     // Case 1:
@@ -49,7 +46,7 @@ void solve() {
     //
     // vals[i] contains the total number of inversions we want to have at positions i.
     //
-    // Call the set of all values which haven't been chosen yet U.
+    // Call the set of all values which haven't been chosen OR which we haven't seen yet U.
     //
     // There are 3 types of inversions:
     // 1. inversions in [0, j], which we don't care about, call it T1
@@ -79,7 +76,167 @@ void solve() {
     //
     // H_x - 2G_x = vals[i] - T1 - T2 - T3 - L
 
+    auto change = [](auto& seg, int p, int v) {
+        int n = (int)seg.size() / 2;
+        p += n;
 
+        int delta = v - seg[p];
+        seg[p] = v;
 
+        while (p > 1) {
+            p /= 2;
+            seg[p] += delta;
+        }
+    };
 
+    auto get = [](auto& seg, int l, int r) {
+        int res = 0;
+        int n = seg.size() / 2;
+
+        for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+            if (l & 1) {
+                res += seg[l++];
+            }
+            if (r & 1) {
+                res += seg[--r];
+            }
+        }
+
+        return res;
+    };
+
+    auto kth = [get](auto& seg, int val) {
+        int n = seg.size() / 2;
+        int p = 1;
+
+        while (p < n) {
+            int left = 2 * p;
+
+            if (val < seg[left]) {
+                p = left;
+            } else {
+                val -= seg[left];
+                p = left + 1;
+            }
+        }
+
+        return p - n;
+    };
+
+    auto size_ = [](auto& seg) {
+        return seg[1];
+    };
+
+    int sz = 1;
+    while (sz < n) {
+        sz <<= 1;
+    }
+
+    vector<int> U(2*sz);
+
+    for (int i = 0; i < n; ++i) {
+        U[sz + i] = 1;
+    }
+
+    for (int i = sz - 1; i >= 1; --i) {
+        U[i] = U[2 * i] + U[2 * i + 1];
+    }
+
+    auto case_1 = [&](int i) {
+        int greater = vals[i] - vals[i - 1];
+        int pos = kth(U, size_(U) - greater - 1);
+        change(U, pos, 0);
+        return pos + 1;
+    };
+
+    vector<int> seg(2*n);
+
+    auto case_2 = [&](int i) {
+        int j = i - 1;
+
+        vector<int> p_block;
+
+        while (j >= 0 && type[j] == 'p') {
+            int x = (int)vals[j] - 1;
+            p_block.push_back(x);
+            change(U, x, 0);
+            j--;
+        }
+
+        if (j == -1) {
+            return kth(U, 0) + 1;
+        }
+
+        std::reverse(p_block.begin(), p_block.end());
+        long long S = vals[i];
+        long long T1 = vals[j];
+        long long T2 = 0;
+        long long T3 = 0;
+        long long L = (int)p_block.size();
+
+        for (int p_i : p_block) {
+            T2 += get(seg, p_i + 1, n);
+            T3 += get(U, p_i + 1, n);
+            change(seg, p_i, 1);
+        }
+
+        // H_x - 2*G_x = S - T1 - T2 - T3 - L
+        long long target = S - T1 - T2 - T3 - L;
+
+        int l = 0;
+        int r = size_(U) - 1;
+
+        auto value = [&](int x) {
+            // G_u - the number of numbers in (j, i) < u
+            // H_u - the number of numbers in U > u
+            long long G_u = get(seg, 0, x);
+            long long H_u = get(U, x + 1, n);
+            return H_u - 2*G_u;
+        };
+
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            int x = kth(U, m);
+
+            if (value(x) <= target) { // FFFTTT
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        int x = kth(U, l);
+        assert(value(x) == target);
+
+        change(U, x, 0);
+        for (int x : p_block) {
+            change(seg, x, 0);
+        }
+
+        return x + 1;
+    };
+
+    vector<int> ans(n);
+
+    for (int i = n - 1; i >= 1; --i) {
+        if (type[i] == 's' && type[i - 1] == 's') {
+            ans[i] = case_1(i);
+        } else if (type[i] == 'p') {
+            ans[i] = vals[i];
+            change(U, vals[i] - 1, 0);
+        } else if (type[i] == 's' && type[i - 1] == 'p') {
+            ans[i] = case_2(i);
+        }
+    }
+
+    if (type[0] == 's') {
+        ans[0] = kth(U, 0) + 1;
+    } else {
+        ans[0] = vals[0];
+    }
+
+    for (int i = 0; i < n; ++i) {
+        cout << ans[i] << ' ';
+    }
+    cout << '\n';
 }
