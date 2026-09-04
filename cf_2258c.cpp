0d77484b70dbd444ee933d1d75e291d2bdd10400
Kirill Katz <xnlyre@protonmail.com>
2026-09-04 19:01:38 +0300

    Upsolve

--- cf_2258c.cpp
+++ cf_2258c.cpp
@@ -20,5 +20,35 @@ int main() {
 }
 
 void solve() {
+    int n;
+    cin >> n;
 
+    auto ask = [&](int u, int v, int d) {
+        cout << '?' << ' ' << u << ' ' << v << ' ' << d << '\n';
+        cout.flush();
+
+        bool ge;
+        cin >> ge;
+        return ge;
+    };
+
+
+    auto find_farthest = [&](int u, int mx = 0, int ans) -> pair<int, int> {
+        for (int v = 1; v <= n; ++v) {
+            if (v == u) continue;
+
+            while (ask(u, v, mx + 1)) {
+                mx++;
+                ans = v;
+            }
+        }
+
+        return { ans, mx };
+    };
+
+    auto [v, mx] = find_farthest(1, 0, 1);
+    auto [u, d] = find_farthest(v, mx, 1);
+
+    cout << '!' << ' ' << v << ' ' << u << ' ' << d << '\n';
+    cout.flush();
 }
