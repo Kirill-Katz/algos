7cec45a40e3408774f7b24e8988786451e28d80e
Kirill Katz <xnlyre@protonmail.com>
2026-09-10 21:13:16 +0300

    Solve

--- cf_2259f.cpp
+++ cf_2259f.cpp
@@ -25,7 +25,12 @@ void solve() {
 
     vector<int> a(n);
     string s;
-    s.resize(n);
+
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+
+    cin >> s;
 
     // this is a block thing game.
     //
@@ -83,4 +88,119 @@ void solve() {
     // 2 -> -2 # increase val by 1
     // Each operation decreases the number of inversions by the sum of values for non zero keys, so we need to keep track of these values.
 
+    long long init = 0;
+    int o = 0;
+
+    for (int i = 0; i < n; ++i) {
+        if (a[i] == 1) {
+            o++;
+        } else {
+            init += o;
+        }
+    }
+
+    deque<pair<int,int>> ones_;
+    deque<pair<int,int>> zeros_;
+
+    long long one_op = 0;
+    long long zero_op = 0;
+
+    for (int i = 0; i < n;) {
+        if (a[i] == 0) {
+            ++i;
+            continue;
+        }
+
+        int os = 0;
+        int zs = 0;
+
+        int j = i;
+
+        while (j < n && a[j] == 1) {
+            os++;
+            j++;
+        }
+
+        while (j < n && a[j] == 0) {
+            zs++;
+            j++;
+        }
+
+        one_op += zs;
+
+        if (zs > 0) {
+            ones_.push_back({ os, zs });
+        }
+
+        i = j;
+    }
+
+    for (int i = n - 1; i >= 0;) {
+        if (a[i] == 1) {
+            --i;
+            continue;
+        }
+
+        int os = 0;
+        int zs = 0;
+
+        int j = i;
+
+        while (j >= 0 && a[j] == 0) {
+            zs++;
+            j--;
+        }
+
+        while (j >= 0 && a[j] == 1) {
+            os++;
+            j--;
+        }
+
+        zero_op += os;
+
+        if (os > 0) {
+            zeros_.push_back({ zs, os });
+        }
+
+        i = j;
+    }
+
+    auto apply = [](auto& main, auto& sec, auto& main_op, auto& sec_op) {
+        if (!main.empty()) {
+            auto& [k, v] = main.front();
+            k--;
+
+            if (k == 0) {
+                main_op -= v;
+                main.pop_front();
+            }
+        }
+
+        if (!sec.empty()) {
+            auto& [k, v] = sec.back();
+            v--;
+            sec_op -= 1;
+
+            if (v == 0) {
+                sec.pop_back();
+            }
+        }
+    };
+
+    cout << init << ' ';
+    for (char c : s) {
+        if (c == '1') {
+            init -= one_op;
+            cout << init << ' ';
+
+            apply(ones_, zeros_, one_op, zero_op);
+        } else {
+            init -= zero_op;
+            cout << init << ' ';
+
+            apply(zeros_, ones_, zero_op, one_op);
+        }
+    }
+
+    cout << '\n';
 }
