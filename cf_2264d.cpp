1b95bb1ee372cdea0c0b0440a2efdd9257fbfa1e
Kirill Katz <xnlyre@protonmail.com>
2026-09-19 22:32:43 +0300

    .

--- cf_2264d.cpp
+++ cf_2264d.cpp
@@ -23,70 +23,90 @@ void solve() {
     int n;
     cin >> n;
 
-    // let's view all the segments of size 1, 2, 3, ...
-    //
-    // for segments of size 1 we have a hard limit of n - 3 wrong segments, because we have only 3 1s in the binary
-    // string
-    //
-    // Assume we can only have 1 ones. Then:
-    // we want to divide the string into two equal parts with 1 at the middle, because
-    // that reduces the number of 0 containing segments.
-    //
-    // Assume we can only have 2 ones. Then:
-    // we want to divide the string into three equal parts with the middle part having an odd number
-    // of zeros. This is because an even number of zeros between them causes the middle segment to become divisible by 3.
-    //
-    // Assume we can only have 3 ones. Then - it is hard
-    // - we probably want the "base string" to not be divisible by 3.
-    // - if the base string is not divisible by 3 then we are forced to get 2 substrings divisible by
-    // - we want to have 4 segments containing zeros
-    // -
-    //
-    // maybe 2 ones is as good as it gets?
-    //
-    // the problem is that with 2 ones we have only 3 segments were we have n^2 explosion of bad segments,
-    // while with 3 ones, each segment is smaller, but now the overall structure is not nice, and we are forced
-    // to have n^2 explosions containings 1.
+    // [_____] 1 [_____] 1 [_____] 1 [______]
+    //    x         y         z         w
 
-    if (n == 1) {
-        cout << "1" << '\n';
-        return;
+    int base = (n - 3) / 4;
+
+    long long zero_bad = [&](long long x, long long y, long long z, long long w) {
+        return 1LL * x * (x + 1) / 2 +
+        1LL * y * (y + 1) / 2 +
+        1LL * z * (z + 1) / 2 +
+        1LL * w * (w + 1) / 2;
+    };
+
+    string construct_ans = [&](long long x, long long y, long long z, long long w) {
+        string ans(n, "0");
+        ans[x] = '1';
+        ans[y + x + 1] = '1';
+        ans[y + x + z + 2] = '1';
+        ans[y + x + z + w + 3] = '1';
+
+        return ans;
+    };
+
+    vector<pair<long long, string>> ans;
+
+    // even, even
+    {
+        long long y = base;
+        long long z = base;
+
+        if (y & 1) {
+            y++;
+        }
+
+        if (z & 1) {
+            z++;
+        }
+
+        long long x = ((n - 3) - y - z) / 2;
+        long long w = (n - 3) - x - y - z;
+
+        long long even_even_add = (x + 1) * (z + 1) + (y + 1) * (w + 1);
+        ans.push_back({ even_even_add + zero_bad(x, y, z, w), construct_ans(x, y, z, w) };
     }
 
-    if (n == 2) {
-        cout << "11" << '\n';
-        return;
+    // odd, even
+    {
+        long long y = base;
+        long long z = base;
+
+        if (!(y & 1)) {
+            y++;
+        }
+
+        if (z & 1) {
+            z++;
+        }
+
+        long long x = ((n - 3) - y - z) / 2;
+        long long w = (n - 3) - x - y - z;
+
+        long long odd_even_add = (y + 1) * (w + 1);
+        ans.push_back({ odd_even_add + zero_bad(x, y, z, w), construct_ans(x, y, z, w) };
     }
 
-    int total = n - 2;
+    // even, odd
+    {
+        long long y = base;
+        long long z = base;
 
-    int mid = total / 3;
+        if (y & 1) {
+            y++;
+        }
 
-    if (!(mid & 1)) {
-        mid++;
+        if (!(z & 1)) {
+            z++;
+        }
+
+        long long x = ((n - 3) - y - z) / 2;
+        long long w = (n - 3) - x - y - z;
+
+        long long even_odd_add = (x + 1) * (z + 1);
+        ans.push_back({ even_odd_add + zero_bad(x, y, z, w), construct_ans(x, y, z, w) };
     }
 
-    int left = (total - mid) / 2;
-    int right = total - left - mid;
 
-    string s = "";
-
-    for (int i = 0; i < left; ++i) {
-        s.push_back('0');
-    }
-
-    s.push_back('1');
-
-    for (int i = 0; i < mid; ++i) {
-        s.push_back('0');
-    }
-
-    s.push_back('1');
-
-    for (int i = 0; i < right; ++i) {
-        s.push_back('0');
-    }
-
-    cout << s << '\n';
 }
 
